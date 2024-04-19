ff86f635bd0218087c08b5a2bc2b5b190c3c8fb1
김수민 <ksm6644998@gmail.com>
2024-04-19 16:10:44 +0900

    a
    a

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def root():
-    return { "message": "Hello World!"}
+def read_root():
+    return {"Hello": "World"}
 
-@app.get("/home")
-def home():
-    return { "message": "Home!"}
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str,None] = None):
+    return {"item_id": item_id, "q": q}
